821e5892cf4646806d8bd8fe3c9d8ab88a9e5714
Massalin <May.massalinlawoo@gmail.com>
2018-02-19 14:47:13 +0700

    User Login with sqlite

--- src/sample/loginModel.java
+++ src/sample/loginModel.java
@@ -1,7 +1,6 @@
 package sample;
 
 
-                import dbUtil.dbConnection;
                 import java.sql.Connection;
                 import java.sql.PreparedStatement;
                 import java.sql.ResultSet;
